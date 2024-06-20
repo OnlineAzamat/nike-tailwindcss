2abe32c8dc4c02458942f48aa0713f19cc086dc0
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-06-20 22:53:18 +0500

    Add assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+  </main>
+);
+
+export default App;
